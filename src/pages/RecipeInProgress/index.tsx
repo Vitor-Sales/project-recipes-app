7caac0802f0544171fc3e209c40aa6db6ccd2afd
Fabio Alencar <devfga@gmail.com>
2024-04-12 15:53:55 -0300

    falta terminar teste butao de finalizar

--- src/pages/RecipeInProgress/index.tsx
+++ src/pages/RecipeInProgress/index.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable max-lines */
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-// eslint-disable-next-line sonarjs/cognitive-complexity
 function RecipeInProgress() {
   const [recipeType, setRecipeType] = useState<string>('');
   const [recipeId, setRecipeId] = useState<string>('');
@@ -12,142 +10,39 @@ function RecipeInProgress() {
   const [isRecipeFavorited, setIsRecipeFavorited] = useState<boolean>();
   const [checkedIngredients, setCheckedIngredients] = useState<Set<string>>(new Set());
   const navigate = useNavigate();
+  const inputCheckbox = 'input[type="checkbox"]';
 
-  // remove mock later
-  const mockObj = {
-    drinks: {
-      178319: {
-        strDrink: 'Aquamarine',
-        strDrinkAlternate: null,
-        strTags: null,
-        strVideo: null,
-        strCategory: 'Cocktail',
-        strIBA: null,
-        strAlcoholic: 'Alcoholic',
-        strGlass: 'Martini Glass',
-        strInstructions: 'Shake well in a shaker with ice.\r\nStrain in a martini glass.',
-        strInstructionsES: 'Agite bien en una coctelera con hielo. Cuela en una copa de Martini.',
-        strInstructionsDE: null,
-        strInstructionsFR: null,
-        strInstructionsIT: 'Shakerare bene in uno shaker con ghiaccio.\r\nFiltrare in una coppetta Martini.',
-        'strInstructionsZH-HANS': null,
-        'strInstructionsZH-HANT': null,
-        strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-        strIngredient1: 'Hpnotiq',
-        strIngredient2: 'Pineapple Juice',
-        strIngredient3: 'Banana Liqueur',
-        strIngredient4: null,
-        strIngredient5: null,
-        strIngredient6: null,
-        strIngredient7: null,
-        strIngredient8: null,
-        strIngredient9: null,
-        strIngredient10: null,
-        strIngredient11: null,
-        strIngredient12: null,
-        strIngredient13: null,
-        strIngredient14: null,
-        strIngredient15: null,
-        strMeasure1: '2 oz',
-        strMeasure2: '1 oz',
-        strMeasure3: '1 oz',
-        strMeasure4: '',
-        strMeasure5: '',
-        strMeasure6: '',
-        strMeasure7: '',
-        strMeasure8: null,
-        strMeasure9: null,
-        strMeasure10: null,
-        strMeasure11: null,
-        strMeasure12: null,
-        strMeasure13: null,
-        strMeasure14: null,
-        strMeasure15: null,
-        strImageSource: null,
-        strImageAttribution: null,
-        strCreativeCommonsConfirmed: 'No',
-        dateModified: null,
-      },
-    },
-    meals: {
-      52771: {
-        strMeal: 'Spicy Arrabiata Penne',
-        strDrinkAlternate: null,
-        strCategory: 'Vegetarian',
-        strArea: 'Italian',
-        strInstructions: 'Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.',
-        strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-        strTags: 'Pasta,Curry',
-        strYoutube: 'https://www.youtube.com/watch?v=1IszT_guI08',
-        strIngredient1: 'penne rigate',
-        strIngredient2: 'olive oil',
-        strIngredient3: 'garlic',
-        strIngredient4: 'chopped tomatoes',
-        strIngredient5: 'red chile flakes',
-        strIngredient6: 'italian seasoning',
-        strIngredient7: 'basil',
-        strIngredient8: 'Parmigiano-Reggiano',
-        strIngredient9: '',
-        strIngredient10: '',
-        strIngredient11: '',
-        strIngredient12: '',
-        strIngredient13: '',
-        strIngredient14: '',
-        strIngredient15: '',
-        strIngredient16: null,
-        strIngredient17: null,
-        strIngredient18: null,
-        strIngredient19: null,
-        strIngredient20: null,
-        strMeasure1: '1 pound',
-        strMeasure2: '1/4 cup',
-        strMeasure3: '3 cloves',
-        strMeasure4: '1 tin ',
-        strMeasure5: '1/2 teaspoon',
-        strMeasure6: '1/2 teaspoon',
-        strMeasure7: '6 leaves',
-        strMeasure8: 'spinkling',
-        strMeasure9: '',
-        strMeasure10: '',
-        strMeasure11: '',
-        strMeasure12: '',
-        strMeasure13: '',
-        strMeasure14: '',
-        strMeasure15: '',
-        strMeasure16: null,
-        strMeasure17: null,
-        strMeasure18: null,
-        strMeasure19: null,
-        strMeasure20: null,
-        strSource: null,
-        strImageSource: null,
-        strCreativeCommonsConfirmed: null,
-        dateModified: null,
-      },
-    },
+  const fetchRecipe = async (type: string, id: string) => {
+    let apiUrl;
+    if (type === 'meals') {
+      apiUrl = 'www.themealdb.com';
+    } else if (type === 'drinks') {
+      apiUrl = 'www.thecocktaildb.com';
+    }
+    try {
+      const response = await fetch(`https://${apiUrl}/api/json/v1/1/lookup.php?i=${id}`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch recipe data');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching recipe:', error);
+      throw error;
+    }
   };
 
-  const setUpData = () => {
-    localStorage.setItem('inProgressRecipes', JSON.stringify(mockObj));
-  };
-
-  const getRecipeInfo = () => {
-    // get the type from the url
+  const getRecipeInfo = async () => {
     const path = window.location.pathname;
     const regex = /\/(meals|drinks)\/(\d+)\/in-progress/;
     const match = path.match(regex);
     if (match) {
       const [, type, id] = match;
-      // get the recipe info from localstorage
-      const storedDataString = localStorage.getItem('inProgressRecipes');
+      const recipeData = await fetchRecipe(type, id);
+      setRecipe(recipeData[type][0]);
       setRecipeType(type);
-      if (storedDataString) {
-        const storedData = JSON.parse(storedDataString);
-        setRecipe(storedData[type][id]);
-        setRecipeId(id);
-      }
+      setRecipeId(id);
     }
-    // get the checked ingredients from localstorage
     const storedCheckedIngredients = localStorage.getItem('checkedIngredients');
     if (storedCheckedIngredients) {
       setCheckedIngredients(new Set(JSON.parse(storedCheckedIngredients)));
@@ -155,8 +50,7 @@ function RecipeInProgress() {
   };
 
   useEffect(() => {
-    // Update checked status of checkboxes after recipe state is set
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const checkboxes = document.querySelectorAll(inputCheckbox);
     checkboxes.forEach((checkbox: any) => {
       const ingredientName = checkbox.parentNode.textContent.trim();
       if (checkedIngredients.has(ingredientName)) {
@@ -166,13 +60,8 @@ function RecipeInProgress() {
   }, [recipe]);
 
   useEffect(() => {
-    // set up mock data
-    // can be removed later
-    setUpData();
-
-    // get recipe info from localstorage
     getRecipeInfo();
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const checkboxes = document.querySelectorAll(inputCheckbox);
     checkboxes.forEach((checkbox: any) => {
       const ingredientName = checkbox.parentNode.textContent.trim();
       if (checkedIngredients.has(ingredientName)) {
@@ -194,19 +83,16 @@ function RecipeInProgress() {
     initialFavorite();
   }, [recipeId]);
 
-  // function to iterate ingredients and render them
   const renderIngredients = () => {
     const ingredients = [];
-    // iterate over the ingredients on api reponse
     for (let i = 1; i <= 20; i++) {
       const ingredientKey = `strIngredient${i}` as keyof typeof recipe;
       const ingredientValue = recipe[ingredientKey];
       if (ingredientValue) {
-        const isChecked = checkedIngredients.has(ingredientValue); // Check if ingredient is checked
+        const isChecked = checkedIngredients.has(ingredientValue);
         ingredients.push({ name: ingredientValue, checked: isChecked });
       }
     }
-    // render the ingredients checkboxes
     return ingredients.map((ingredient, index) => {
       return (
         <label key={ index } data-testid={ `${index}-ingredient-step` }>
@@ -220,7 +106,6 @@ function RecipeInProgress() {
     });
   };
 
-  // function to change the checked ingredient decoration and turn the finish btn able
   function toggleTextDecoration(checkbox: any) {
     const isChecked = checkbox?.checked;
     const ingredientText = checkbox?.parentNode as HTMLElement;
@@ -231,25 +116,21 @@ function RecipeInProgress() {
       ingredientText.style.textDecoration = 'none';
     }
     updateCheckedIngredients(ingredientName, isChecked);
-    // check if all ingredients are checked
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    const checkboxes = document.querySelectorAll(inputCheckbox);
     let allIngredientsChecked = true;
     checkboxes.forEach((input: any) => {
       if (!input.checked) {
         allIngredientsChecked = false;
       }
     });
-    // enable finish button if all ingredients are checked
     if (allIngredientsChecked) {
       setIsFinishBtnDisabled(false);
     }
-    // disable finish button if any ingredient is unchecked
     if (!isChecked) {
       setIsFinishBtnDisabled(true);
     }
   }
 
-  // function to save recipe finished and redirect
   const finishRecipe = () => {
     const type = recipeType.slice(0, -1);
     const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes')) || [];
@@ -269,7 +150,6 @@ function RecipeInProgress() {
     navigate('/done-recipes');
   };
 
-  // copy the url to the clipboard and show the Link copied on screen
   const shareRecipe = async () => {
     setLinkCopied(true);
     await navigator.clipboard.writeText(`http://localhost:3000/${recipeType}/${recipeId}`);
@@ -301,7 +181,6 @@ function RecipeInProgress() {
     }
   };
 
-  // Function to update checked ingredients
   const updateCheckedIngredients = (ingredient: string, isChecked: boolean) => {
     const newCheckedIngredients = new Set(checkedIngredients);
     if (isChecked) {
@@ -310,7 +189,6 @@ function RecipeInProgress() {
       newCheckedIngredients.delete(ingredient);
     }
     setCheckedIngredients(newCheckedIngredients);
-    // Save checked ingredients to local storage
     localStorage
       .setItem('checkedIngredients', JSON.stringify(Array.from(newCheckedIngredients)));
   };
@@ -336,8 +214,8 @@ function RecipeInProgress() {
       <button onClick={ () => favoriteRecipe(isRecipeFavorited) }>
         <img
           data-testid="favorite-btn"
-          src={ isRecipeFavorited ? '/src/images/blackHeartIcon.svg?v=1'
-            : '/src/images/whiteHeartIcon.svg?v=1' }
+          src={ isRecipeFavorited ? '/src/images/blackHeartIcon.svg'
+            : '/src/images/whiteHeartIcon.svg' }
           alt="favorite button"
         />
       </button>
@@ -349,7 +227,7 @@ function RecipeInProgress() {
       <button
         data-testid="finish-recipe-btn"
         disabled={ isFinishBtnDisabled }
-        onClick={ finishRecipe }
+        onClick={ () => finishRecipe() }
       >
         Finalizar
       </button>
--- src/tests/RecipeInProgress.test.tsx
+++ src/tests/RecipeInProgress.test.tsx
@@ -1,164 +1,135 @@
-import { screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
+import { it, vi } from 'vitest';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
 
-describe('Recipe in progress tests', () => {
-  const favBtn = 'favorite-btn';
+describe('Testando fetch', () => {
   const route = '/meals/52771/in-progress';
-  const finishBtnId = 'finish-recipe-btn';
   const mealName = 'Spicy Arrabiata Penne';
   const mealThumb = 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg';
-  beforeEach(() => {
-    const mockObj = {
-      drinks: {
-        178319: {
-          strDrink: 'Aquamarine',
-          strDrinkAlternate: null,
-          strTags: null,
-          strVideo: null,
-          strCategory: 'Cocktail',
-          strIBA: null,
-          strAlcoholic: 'Alcoholic',
-          strGlass: 'Martini Glass',
-          strInstructions: 'Shake well in a shaker with ice.\r\nStrain in a martini glass.',
-          strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-          strIngredient1: 'Hpnotiq',
-          strIngredient2: 'Pineapple Juice',
-          strIngredient3: 'Banana Liqueur',
-        },
+  const favBtnId = 'favorite-btn';
+  const blackIcon = '/src/images/blackHeartIcon.svg';
+  const MOCK_RECIPE = {
+    meals: [
+      {
+        idMeal: '52771',
+        strMeal: 'Spicy Arrabiata Penne',
+        strDrinkAlternate: null,
+        strCategory: 'Vegetarian',
+        strArea: 'Italian',
+        strInstructions: 'Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.',
+        strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+        strTags: 'Pasta,Curry',
+        strYoutube: 'https://www.youtube.com/watch?v=1IszT_guI08',
+        strIngredient1: 'penne rigate',
+        strIngredient2: 'olive oil',
+        strIngredient3: 'garlic',
+        strIngredient4: 'chopped tomatoes',
+        strIngredient5: 'red chile flakes',
+        strIngredient6: 'italian seasoning',
+        strIngredient7: 'basil',
+        strIngredient8: 'Parmigiano-Reggiano',
+        strIngredient9: '',
+        strIngredient10: '',
+        strIngredient11: '',
+        strIngredient12: '',
+        strIngredient13: '',
+        strIngredient14: '',
+        strIngredient15: '',
+        strIngredient16: null,
+        strIngredient17: null,
+        strIngredient18: null,
+        strIngredient19: null,
+        strIngredient20: null,
+        strMeasure1: '1 pound',
+        strMeasure2: '1/4 cup',
+        strMeasure3: '3 cloves',
+        strMeasure4: '1 tin ',
+        strMeasure5: '1/2 teaspoon',
+        strMeasure6: '1/2 teaspoon',
+        strMeasure7: '6 leaves',
+        strMeasure8: 'spinkling',
+        strMeasure9: '',
+        strMeasure10: '',
+        strMeasure11: '',
+        strMeasure12: '',
+        strMeasure13: '',
+        strMeasure14: '',
+        strMeasure15: '',
+        strMeasure16: null,
+        strMeasure17: null,
+        strMeasure18: null,
+        strMeasure19: null,
+        strMeasure20: null,
+        strSource: null,
+        strImageSource: null,
+        strCreativeCommonsConfirmed: null,
+        dateModified: null,
       },
-      meals: {
-        52771: {
-          strMeal: mealName,
-          strDrinkAlternate: null,
-          strCategory: 'Vegetarian',
-          strArea: 'Italian',
-          strInstructions: 'Bring a large pot of water to a boil. Add kosher salt to the boiling water, then add the pasta. Cook according to the package instructions, about 9 minutes.\r\nIn a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, 1 to 2 minutes. Add the chopped tomatoes, red chile flakes, Italian seasoning and salt and pepper to taste. Bring to a boil and cook for 5 minutes. Remove from the heat and add the chopped basil.\r\nDrain the pasta and add it to the sauce. Garnish with Parmigiano-Reggiano flakes and more basil and serve warm.',
-          strMealThumb: mealThumb,
-          strTags: 'Pasta,Curry',
-          strIngredient2: 'olive oil',
-          strIngredient3: 'garlic',
-          strIngredient4: 'chopped tomatoes',
-          strIngredient5: 'red chile flakes',
-          strIngredient6: 'italian seasoning',
-          strIngredient7: 'basil',
-          strIngredient8: 'Parmigiano-Reggiano',
-        },
-      },
-    };
-    localStorage.setItem('inProgressRecipes', JSON.stringify(mockObj));
+    ],
+  };
+  const MOCK_RESPONSE = {
+    ok: true,
+    status: 200,
+    json: async () => MOCK_RECIPE,
+  } as Response;
+
+  beforeEach(async () => {
+    vi.spyOn(global, 'fetch').mockResolvedValue(MOCK_RESPONSE);
+    await act(async () => {
+      renderWithRouter(<App />, { route });
+    });
+    window.localStorage.clear();
   });
 
-  afterEach(() => { window.localStorage.clear(); });
+  afterEach(() => vi.clearAllMocks());
 
-  it('Rederiza a página de receita em progresso corretamente para alimentos', () => {
-    renderWithRouter(<App />, { route });
-    expect(screen.getByText(mealName)).toBeInTheDocument();
-    expect(screen.getByTestId('share-btn')).toBeInTheDocument();
-    expect(screen.getByTestId(favBtn)).toBeInTheDocument();
-    expect(screen.getByTestId('recipe-category')).toBeInTheDocument();
-    expect(screen.getByTestId('instructions')).toBeInTheDocument();
-    expect(screen.getAllByRole('checkbox')).toHaveLength(8);
-    expect(screen.getByTestId(finishBtnId)).toBeInTheDocument();
-  });
-  it('Rederiza a página de receita em progresso corretamente para bebidas', () => {
-    renderWithRouter(<App />, { route: '/drinks/178319/in-progress' });
-    expect(screen.getByText('Aquamarine')).toBeInTheDocument();
-    expect(screen.getByTestId('share-btn')).toBeInTheDocument();
-    expect(screen.getByTestId(favBtn)).toBeInTheDocument();
-    expect(screen.getByTestId('recipe-category')).toBeInTheDocument();
-    expect(screen.getByTestId('instructions')).toBeInTheDocument();
-    expect(screen.getAllByRole('checkbox')).toHaveLength(3);
-    expect(screen.getByTestId(finishBtnId)).toBeInTheDocument();
-  });
-  it('Pode marcar e desmarcar os ingredientes mudando sua decoração', async () => {
-    renderWithRouter(<App />, { route });
-    const label = screen.getByTestId('0-ingredient-step');
-    const checkbox = screen.getAllByRole('checkbox')[0];
-    expect(checkbox).not.toBeChecked();
-    await checkbox.click();
-    expect(checkbox).toBeChecked();
-    expect(label).toHaveStyle('text-decoration: line-through solid rgb(0, 0, 0)');
-    await checkbox.click();
-    expect(checkbox).not.toBeChecked();
-    expect(label).toHaveStyle('text-decoration: none');
-  });
-  it('Salva os checkboxes marcados no localStorage e os recupera ao atualizar a página', () => {
-    const checkedIngredients = [
-      'penne rigate',
-      'olive oil',
-      'garlic',
-      'chopped tomatoes',
-      'red chile flakes',
-      'italian seasoning',
-      'basil',
-      'Parmigiano-Reggiano',
-    ];
-    renderWithRouter(<App />, { route });
-    const checkboxes = screen.getAllByRole('checkbox');
-    checkboxes.forEach((checkbox) => {
-      checkbox.click();
+  it('Renderiza as informaçoes da receita', async () => {
+    const recipeImage = await screen.findByTestId('recipe-photo');
+    const recipeName = await screen.findByText(mealName);
+    const recipeCategory = await screen.getByText(/vegetarian/i);
+    const recipeInstructions = await screen.getByTestId('instructions');
+    waitFor(() => {
+      expect(recipeImage).not.toBeInTheDocument();
+      expect(recipeImage).toHaveAttribute(
+        'src',
+        mealThumb,
+      );
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(recipeName).toBeInTheDocument();
+      expect(recipeCategory).toBeInTheDocument();
+      expect(recipeInstructions).toBeInTheDocument();
     });
-    window.location.reload();
-    checkboxes.forEach((checkbox) => {
-      expect(checkbox).toBeChecked();
-    });
-    let checkedIngredientsLocalStorage = JSON.parse(localStorage.getItem('checkedIngredients'));
-    expect(checkedIngredientsLocalStorage).toEqual(checkedIngredients);
-    checkboxes.forEach((checkbox) => {
-      checkbox.click();
-    });
-    window.location.reload();
-    checkboxes.forEach((checkbox) => {
-      expect(checkbox).not.toBeChecked();
-    });
-    checkedIngredientsLocalStorage = JSON.parse(localStorage.getItem('checkedIngredients'));
-    expect(checkedIngredientsLocalStorage).toEqual([]);
   });
-  it('O botao de finalizar fica disponível ao marcar todos os ingredientes, salva a receita e redireciona corretamente', async () => {
-    renderWithRouter(<App />, { route });
-    const checkboxes = screen.getAllByRole('checkbox');
-    const finishBtn = screen.getByTestId(finishBtnId);
-    expect(finishBtn).toBeDisabled();
-    checkboxes.forEach((checkbox) => {
-      checkbox.click();
+  it('Renderiza os ingredientes da receita', async () => {
+    const recipeIngredients = await screen.getAllByRole('checkbox');
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    expect(recipeIngredients).toHaveLength(8);
+  });
+  it('Renderiza os botões de ação', async () => {
+    const shareBtn = await screen.getByTestId('share-btn');
+    const favBtn = await screen.getByTestId(favBtnId);
+    const finishBtn = await screen.getByTestId('finish-recipe-btn');
+    expect(shareBtn).toBeInTheDocument();
+    expect(favBtn).toBeInTheDocument();
+    expect(finishBtn).toBeInTheDocument();
+  });
+  it('Testa a funcionalidade do botao de compartilhar receita', async () => {
+    const shareBtn = await screen.getByTestId('share-btn');
+    expect(shareBtn).toHaveAttribute('src', '/src/images/shareIcon.svg');
+    await act(async () => {
+      shareBtn.click();
+      waitFor(() => {
+        expect(screen.getByText('Link copied!')).toBeInTheDocument();
+        expect(navigator.clipboard.readText()).resolves.toBe('http://localhost:3000/meals/52771');
+      });
     });
-    expect(finishBtn).toBeEnabled();
-    await finishBtn.click();
-    expect(window.location.pathname).toBe('/done-recipes');
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const { doneDate } = doneRecipes[0];
-    const expectedDoneRecipes = {
-      id: '52771',
-      nationality: 'Italian',
-      name: mealName,
-      category: 'Vegetarian',
-      image: mealThumb,
-      tags: [
-        'Pasta',
-        'Curry',
-      ],
-      alcoholicOrNot: '',
-      type: 'meal',
-      doneDate,
-    };
-
-    expect(doneRecipes[0]).toEqual(expectedDoneRecipes);
   });
-  it('O botao de compartilhar copia o link para o clipboard e mostra Link copied!', async () => {
-    renderWithRouter(<App />, { route });
-    const shareBtn = screen.getByTestId('share-btn');
-    await shareBtn.click();
-    expect(screen.getByText('Link copied!')).toBeInTheDocument();
-    expect(navigator.clipboard.readText()).resolves.toBe('http://localhost:3000/meals/52771');
-  });
-  it('O botao de favoritar salva e remove a receita corretamente no localstorage', async () => {
-    renderWithRouter(<App />, { route });
-    const favoriteBtn = screen.getByTestId(favBtn);
-    expect(favoriteBtn).toHaveAttribute('src', '/src/images/whiteHeartIcon.svg?v=1');
-    await favoriteBtn.click();
-    expect(favoriteBtn).toHaveAttribute('src', '/src/images/blackHeartIcon.svg?v=1');
-    let favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  it('Testa a funcionalidade do botao de favoritar receita', async () => {
+    const favBtn = await screen.getByTestId(favBtnId);
+    let favoritesRecipes = JSON.parse(localStorage.getItem('favoriteRecipe') || '[]');
+    expect(favoritesRecipes).toHaveLength(0);
+    expect(favBtn).toHaveAttribute('src', '/src/images/whiteHeartIcon.svg');
     const expectedFavoriteRecipes = {
       id: '52771',
       nationality: 'Italian',
@@ -168,10 +139,52 @@ describe('Recipe in progress tests', () => {
       alcoholicOrNot: '',
       type: 'meal',
     };
-    expect(favoriteRecipes[0]).toEqual(expectedFavoriteRecipes);
-    expect(favoriteRecipes).toHaveLength(1);
-    await favoriteBtn.click();
-    favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
-    expect(favoriteRecipes).toHaveLength(0);
+    await act(async () => {
+      favBtn.click();
+      waitFor(() => {
+        expect(favBtn).toHaveAttribute('src', blackIcon);
+        favoritesRecipes = JSON.parse(localStorage.getItem('favoriteRecipe') || '[]');
+        expect(favoritesRecipes).toHaveLength(1);
+        expect(favoritesRecipes[0]).toEqual(expectedFavoriteRecipes);
+      });
+    });
+  });
+  it('Testa a funcionalidade de marcar e desmamcar ingredientes, mudando sua decoração', async () => {
+    const label = await screen.getByTestId('0-ingredient-step');
+    const checkbox = await screen.getAllByRole('checkbox')[0];
+    await act(async () => {
+      checkbox.click();
+      expect(checkbox).toBeChecked();
+      expect(label).toHaveStyle('text-decoration: line-through solid rgb(0, 0, 0)');
+      checkbox.click();
+      expect(checkbox).not.toBeChecked();
+      expect(label).toHaveStyle('text-decoration: none');
+    });
+  });
+  it('Testa se as informacoes de favoritos e ingredientes marcados são mantidos ao atualizar a página', async () => {
+    const checkboxes = await screen.getAllByRole('checkbox');
+    const favBtn = await screen.getByTestId(favBtnId);
+    checkboxes.forEach((checkbox) => {
+      checkbox.click();
+    });
+    favBtn.click();
+    await waitFor(() => {
+      expect(favBtn).toHaveAttribute('src', blackIcon);
+    });
+    window.location.reload();
+    await waitFor(() => {
+      checkboxes.forEach((checkbox) => {
+        expect(checkbox).toBeChecked();
+      });
+      expect(favBtn).toHaveAttribute('src', blackIcon);
+    });
+  });
+  it('Testa a funcionalidade do botao de finalizar receita', async () => {
+    const finishBtn = await screen.findByTestId('finish-recipe-btn');
+    expect(finishBtn).toBeDisabled();
+    const checkboxes = await screen.getAllByRole('checkbox');
+    checkboxes.forEach((checkbox) => {
+      checkbox.click();
+    });
   });
 });
