af159163f3e4d16bf9f9c7899d2458256062bfef
Bruce <andrebgsi@gmail.com>
2024-04-13 10:17:48 -0300

    Restaura package lock 03

--- src/tests/FavoriteRecipes.test.tsx
+++ src/tests/FavoriteRecipes.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-import FavoriteRecipes from '../../pages/FavoriteRecipes';
+import FavoriteRecipes from '../pages/FavoriteRecipes';
 
 const renderWithRouter = () => {
   return render(
